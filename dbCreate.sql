5f2cb8232e91c93fc2d0d50baa45aa009acb67d7
greatmjh <66207287+greatmjh@users.noreply.github.com>
2026-05-13 15:28:08 +0200

    trigger to delete items_donor records with 0 quantity

--- dbCreate.sql
+++ dbCreate.sql
@@ -62,6 +62,30 @@ CREATE TABLE IF NOT EXISTS items_donor(
 		FOREIGN KEY(item_class) 
 		REFERENCES item_tree(item_id)
 );
+-- to handle deletion of items for donation
+CREATE OR REPLACE FUNCTION fn_drop_zero_idonor()
+RETURNS TRIGGER LANGUAGE plpgsql
+AS $$BEGIN
+	IF (NEW.qty <= 0) THEN 
+		DELETE FROM items_donor WHERE item_code = NEW.item_code;
+	END IF;
+	RETURN NEW;
+END$$;
+
+CREATE TRIGGER trg_drop_zero_idonor
+AFTER UPDATE ON general_requests
+FOR EACH ROW
+EXECUTE FUNCTION fn_drop_zero_idonor();
+
+CREATE TABLE IF NOT EXISTS session_keys (
+	session_key uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	user_id uuid NOT NULL,
+
+	CONSTRAINT fk_user_id
+		FOREIGN KEY (user_id)
+		REFERENCES users(user_id)
+		ON DELETE CASCADE
+);
 
 
 CREATE TABLE IF NOT EXISTS requests(
